79eb6c2cb16aafe8c1b3ccab888b9bee2f150406
Nihal Erdal <nihalerdall@gmail.com>
2021-05-11 16:43:57 -0700

    added search movie feature

--- TheMovieManager/Model/TMDB Client/TMDBClient.swift
+++ TheMovieManager/Model/TMDB Client/TMDBClient.swift
@@ -30,6 +30,7 @@ class TMDBClient {
         case webAuth
         case logout
         case getFavorites
+        case search(String)
         
         var stringValue: String {
             switch self {
@@ -40,6 +41,7 @@ class TMDBClient {
             case .webAuth: return "https://www.themoviedb.org/authenticate/" + Auth.requestToken + "?redirect_to=themoviemanager:authenticate"
             case .logout: return Endpoints.base + "/authentication/session" + Endpoints.apiKeyParam
             case .getFavorites: return Endpoints.base + "/account/\(Auth.accountId)/favorite/movies" + Endpoints.apiKeyParam + "&session_id=\(Auth.sessionId)"
+            case .search(let query): return Endpoints.base + "/search/movie" + Endpoints.apiKeyParam + "&query=\(query)"
             }
         }
         
@@ -100,6 +102,16 @@ class TMDBClient {
         task.resume()
     }
 
+    class func search(query: String, completion: @escaping ([Movie], Error?) -> Void){
+        taskForGETRequest(url: Endpoints.search(query).url, responseType: MovieResults.self) { response, error in
+            if let response = response {
+                completion(response.results, nil)
+            }else{
+               completion([], error)
+            }
+        }
+    }
+    
     
     class func getWatchlist(completion: @escaping ([Movie], Error?) -> Void) {
         taskForGETRequest(url: Endpoints.getWatchlist.url, responseType: MovieResults.self) { (response, error)  in
